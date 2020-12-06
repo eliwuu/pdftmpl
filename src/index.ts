139842e17dcaa51c355deeae2072a3fefcc4c6a0
eliwuu <marekwilk@me.com>
2020-12-06 02:56:41 +0100

    rc 0.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import gitInit from "./gitInit";
 import TemplateBuilder from "./TemplateBuilder";
 
 async function main() {
     const cwd = process.cwd();
 
-    await gitInit(cwd);
     await TemplateBuilder.Make(cwd);
 }
 
