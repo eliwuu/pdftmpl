1fc83ef9c3e89153fd1c4933c9993f733e4d68e3
eliwuu <marekwilk@me.com>
2020-12-24 21:33:13 +0100

    parameter naming

--- src/giti.init.ts
+++ src/giti.init.ts
@@ -1,18 +1,18 @@
 import { execSync } from "child_process";
 import path from "path";
 
-function gitInit(repoName: string, dirPath: string) {
+function gitInit(repoName: string) {
   const currentDir = process.cwd();
-  process.chdir(path.join(currentDir, dirPath));
+  process.chdir(path.join(currentDir, repoName));
   execSync("git init");
   process.chdir(currentDir);
 }
 
-function gitAdd(dirPath: string) {
+function gitAdd(templateName: string) {
   const currentDir = process.cwd();
-  process.chdir(path.join(process.cwd(), dirPath));
+  process.chdir(path.join(process.cwd(), templateName));
   execSync("git add .");
-  execSync("git commit");
+  execSync("git commit -m \"created\"");
 
   process.chdir(currentDir);
 }
